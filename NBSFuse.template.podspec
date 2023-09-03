b2f5def0abfec0aba6aca1e530f13e75ac91a632
Norman Breau <norman@nbsolutions.ca>
2023-09-03 18:10:25 -0300

    test: release script

---
+++ NBSFuse.template.podspec
@@ -0,0 +1,23 @@
+
+Pod::Spec.new do |s|
+  s.name             = 'NBSFuse'
+  s.version          = ':VERSION:'
+  s.summary          = 'NBS Fuse'
+
+  s.description      = <<-DESC
+A native-first framework for building hybrid native-web applications.
+                       DESC
+
+  s.homepage         = 'https://github.com/nbsfuse/fuse'
+  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.license          = { :type => 'Apache-2.0', :file => 'LICENSE' }
+  s.author           = { 'Norman Breau' => 'norman@nbsolutions.ca' }
+  s.source           = {
+    :http => 'https://github.com/nbsfuse/fuse-ios/releases/download/:VERSION:/NBSFuse.xcframework.tar.gz',
+    sha1 => ':CHECKSUM:'
+  }
+
+  s.ios.deployment_target = '13.0'
+
+  s.vendored_frameworks = 'NBSFuse.xcframework'
+end
---
+++ makeRelease.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+# Copyright 2023 Norman Breau 
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source compiler/_assertCleanWorkspace.sh
+source compiler/_assertMac.sh
+
+VERSION="$1"
+
+if [ -z "$VERSION" ]; then
+    echo "Version is required."
+    exit 2
+fi
+
+cd ..
+source compiler/_assertGitTag.sh
+cd fuse-ios
+
+echo $VERSION > VERSION
+
+cd ..
+./build.sh ios
+cd fuse-ios
+
+rm -f ./build/NBSFuse.xcframework.zip
+rm -f ./build/NBSFuse-debug.xcframework.zip
+
+zip ./build/NBSFuse.xcframework.zip -r ./build/NBSFuse.xcframework
+zip ./build/NBSFuse-debug.xcframework.zip -r ./build/NBSFuse-debug.xcframework
+
+CHECKSUM="$(shasum -a 1 build/NBSFuse.xcframework.zip  | cut -d ' ' -f 1)"
+
+# Generate the podspec. Unfortunately due to how podspecs work, they need access to any files
+# they reference, so we can't read from IOS_VERSION in the podspec itself.
+echo "# This is a generated file, do not modify directory\n\n" > NBSFuse.podspec
+echo "$(cat NBSFuse.template.podspec)" >> NBSFuse.podspec
+sed -i '' "s/:VERSION:/$VERSION/g" NBSFuse.podspec
+sed -i '' "s/:CHECKSUM:/$CHECKSUM/g" NBSFuse.podspec
+
+# cd ios/fuse-ios-dist
+# git add .
+# git commit -m "iOS Dist: $VERSION"
+# git tag -a "$VERSION" -m "iOS Dist: $VERSION"
+# git push
+# git push --tags
+# cd ../..
+
+# git add IOS_VERSION ios/fuse-ios-dist
+git commit -m "iOS Release: $VERSION"
+git push
+git tag -a $VERSION -m "iOS Release: $VERSION"
+git push --tags
+
+gh release create $VERSION \
+    ./build/NBSFuse.xcframework.zip \
+    ./build/NBSFuse-debug.xcframework.zip \
+    --verify-tag --generate-notes
+
+pod repo push nbs NBSFuse.podspec
