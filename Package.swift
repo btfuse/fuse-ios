08cb57c9286d5f0223f3559ad7030d64979c7d87
Norman Breau <norman@nbsolutions.ca>
2023-09-10 18:50:49 -0300

    iOS Release: 0.2.17

--- Package.swift
+++ Package.swift
@@ -1,4 +1,23 @@
-// swift-tools-version:5.8
+// This is a generated file, do not modify directory
+
+
+// swift-tools-version: 5.8
+
+/*
+Copyright 2023 Norman Breau
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 import PackageDescription
 
@@ -15,29 +34,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NBSFuse",
-            path: "https://github.com/nbsfuse/fuse-ios/releases/download/0.2.15/NBSFuse.xcframework.zip",
-            checksum: "a018ec6a1be53401cca4db180b5622d5f11704f1"
-        )
-    ]
-)
-
-import PackageDescription
-
-let package = Package(
-    name: "NBSFuse",
-    platforms: [ .iOS("13.0") ],
-    products: [
-        .library(
-            name: "NBSFuse",
-            targets: ["NBSFuse"]
-        )
-    ],
-    dependencies: [],
-    targets: [
-        .binaryTarget(
-            name: "NBSFuse",
-            path: "https://github.com/nbsfuse/fuse-ios/releases/download/0.2.16/NBSFuse.xcframework.zip",
-            checksum: "08c851e1ae26244364d464bce116a7f02822323c"
+            path: "https://github.com/nbsfuse/fuse-ios/releases/download/0.2.17/NBSFuse.xcframework.zip",
+            checksum: "e8e99327af9583780c8588898308b4ec1b4271e1"
         )
     ]
 )
