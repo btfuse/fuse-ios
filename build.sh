21e38b0a6865c2de44337c0ca1c39ba4284bde38
Norman Breau <norman@nbsolutions.ca>
2023-12-25 11:51:33 -0400

    include BTFuseTestTools in releases

--- build.sh
+++ build.sh
@@ -50,27 +50,42 @@ xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuse -configuration Re
 assertLastCall
 xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuse -configuration Debug -destination "generic/platform=iOS Simulator" clean
 assertLastCall
+xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuseTestTools -configuration Release -destination "generic/platform=iOS" clean
+assertLastCall
+xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuseTestTools -configuration Debug -destination "generic/platform=iOS Simulator" clean
+assertLastCall
 
 echo "Building iOS framework..."
 # Now build the iOS platform target in Release mode. We will continue to use Debug mode for iOS Simulator targets.
 xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuse -configuration Release -destination "generic/platform=iOS" build
 assertLastCall
+xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuseTestTools -configuration Release -destination "generic/platform=iOS" build
+assertLastCall
 echo "Building iOS Simulator framework..."
 xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuse -configuration Debug -destination "generic/platform=iOS Simulator" build
+assertLastCall
+xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuseTestTools -configuration Debug -destination "generic/platform=iOS Simulator" build
 assertLastCall
 
 iosBuild=$(echo "$(xcodebuild -workspace BTFuse.xcworkspace -scheme BTFuse -configuration Release -sdk iphoneos -showBuildSettings | grep "CONFIGURATION_BUILD_DIR")" | cut -d'=' -f2 | xargs)
 simBuild=$(echo "$(xcodebuild -workspace BTFuse.xcworkspace -scheme BTFuse -configuration Debug -sdk iphonesimulator -showBuildSettings | grep "CONFIGURATION_BUILD_DIR")" | cut -d'=' -f2 | xargs)
+iosTestToolsBuild=$(echo "$(xcodebuild -workspace BTFuse.xcworkspace -scheme BTFuseTestTools -configuration Release -sdk iphoneos -showBuildSettings | grep "CONFIGURATION_BUILD_DIR")" | cut -d'=' -f2 | xargs)
+simTestToolsBuild=$(echo "$(xcodebuild -workspace BTFuse.xcworkspace -scheme BTFuseTestTools -configuration Debug -sdk iphonesimulator -showBuildSettings | grep "CONFIGURATION_BUILD_DIR")" | cut -d'=' -f2 | xargs)
 
 echo "Signing iOS build..."
 codesign -s $BTFUSE_CODESIGN_IDENTITY --deep $iosBuild/BTFuse.framework
+assertLastCall
+codesign -s $BTFUSE_CODESIGN_IDENTITY --deep $iosTestToolsBuild/BTFuseTestTools.framework
 assertLastCall
 
 echo "Verifying iOS Build"
 codesign -dvvvv $iosBuild/BTFuse.framework
 assertLastCall
+codesign -dvvvv $iosTestToolsBuild/BTFuseTestTools.framework
+assertLastCall
 
 cp -r $iosBuild/BTFuse.framework.dSYM ./dist/
+cp -r $iosTestToolsBuild/BTFuseTestTools.framework.dSYM ./dist
 
 echo "Packing XCFramework..."
 xcodebuild -create-xcframework \
@@ -78,10 +93,21 @@ xcodebuild -create-xcframework \
     -debug-symbols $iosBuild/BTFuse.framework.dSYM \
     -framework $simBuild/BTFuse.framework \
     -output dist/BTFuse.xcframework
+assertLastCall
+xcodebuild -create-xcframework \
+    -framework $iosTestToolsBuild/BTFuseTestTools.framework \
+    -debug-symbols $iosTestToolsBuild/BTFuseTestTools.framework.dSYM \
+    -framework $simTestToolsBuild/BTFuseTestTools.framework \
+    -output dist/BTFuseTestTools.xcframework
+assertLastCall
 
 spushd dist
     zip -r BTFuse.xcframework.zip BTFuse.xcframework > /dev/null
     zip -r BTFuse.framework.dSYM.zip BTFuse.framework.dSYM > /dev/null
+    zip -r BTFuseTestTools.xcframework.zip BTFuseTestTools.xcframework > /dev/null
+    zip -r BTFuseTestTools.framework.dSYM.zip BTFuseTestTools.framework.dSYM > /dev/null
     sha1_compute BTFuse.xcframework.zip
     sha1_compute BTFuse.framework.dSYM.zip
+    sha1_compute BTFuseTestTools.xcframework.zip
+    sha1_compute BTFuseTestTools.framework.dSYM.zip
 spopd
